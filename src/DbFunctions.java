49a0fbb7aad453a0bfb3439e15987be4ae43ae6b
Gustavo Bernardo <gustavobtflores@gmail.com>
2023-06-07 18:26:02 -0300

    init

---
+++ src/DbFunctions.java
@@ -0,0 +1,85 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DbFunctions {
+    public Connection connect_to_db(String dbname, String user, String pass){
+        Connection conn=null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn= DriverManager.getConnection("jdbc:postgresql://localhost:5432/"+dbname, user, pass);
+
+            if(conn!=null){
+                System.out.println("Connection Established");
+            }else {
+                System.out.println("Connection failed");
+            }
+
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+        return conn;
+    }
+
+    public void insert_row(Connection conn,String table_name, String name, String cpf){
+        Statement statement;
+
+        try {
+            String query = String.format("INSERT INTO %s(nome, cpf) values('%s', '%s');", table_name, name, cpf);
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+
+            System.out.println("Row inserted");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void delete_row(Connection conn,String table_name, String cpf){
+        Statement statement;
+
+        try {
+            String query = String.format("DELETE FROM %s WHERE cpf='%s'", table_name, cpf);
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+
+            System.out.println("Row deleted");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void list(Connection conn,String table_name, String cpf){
+        Statement statement;
+
+        try {
+            String query = String.format("SELECT * FROM %s", table_name);
+            statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+
+            while(rs.next()){
+                System.out.println("Nome: " + rs.getString("nome") + ", CPF: " + rs.getString("cpf"));
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void update(Connection conn,String table_name, String cpf){
+        Statement statement;
+
+        try {
+            String query = String.format("UPDATE", table_name, cpf);
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+
+            System.out.println("Row deleted");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        DbFunctions db = new DbFunctions();
+        Connection conn = db.connect_to_db("library", "postgres", "admin");
+        int option = 0;
+
+        while(true){
+            System.out.println("1 - Inserir funcionário\n2 - Deletar funcionário\n3 - Listar funcionário\n4 - Atualizar funcionário");
+            System.out.println("Digite uma opção: ");
+            option = sc.nextInt();
+
+            switch(option){
+                case 1:
+                    sc.nextLine();
+                    System.out.println("Digite o nome do funcionário: ");
+                    String name = sc.nextLine();
+                    System.out.println("Digite o CPF do funcionário: ");
+                    String cpf = sc.nextLine();
+
+                    db.insert_row(conn, "Funcionario", name, cpf);
+                    break;
+                case 2:
+                    sc.nextLine();
+                    System.out.println("Qual o CPF do funcionário que deseja apagar: ");
+                    String cpfToDelete = sc.nextLine();
+                    db.delete_row(conn, "Funcionario", cpfToDelete);
+            }
+        }
+    }
+}
